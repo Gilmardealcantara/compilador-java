862c1f3b49b5e5a0a9a9310139f451ccdd0de048
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-09-07 23:12:25 -0300

    Add lexer teste.

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,13 @@ import lexico.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		Lexer lexer = new Lexer("imputs/teste1.txt");
+		Token tok = null;
+		
+		do{
+			tok = lexer.scan();
+			System.out.println(tok.toString());
+
+		}while(tok.tag != 65535);//????? funfa
 		
 	}
 }
