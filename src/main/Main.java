156112f046f0e01e310a91fa85afacfd26c4ebe6
Gilmardealcantara <gilmardealcantara@gmail.com>
2016-09-11 23:13:30 -0300

    test management.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,29 @@
 package main;
 
+import java.util.Scanner;
 import java.io.IOException;
 
 import lexico.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
+	
 		
-		Lexer lexer = new Lexer("inputs/teste2.txt");
-		Token tok = null;
-		
-		do{
-			tok = lexer.scan();
-			System.out.println(tok.toString());
-
-		}while(tok.tag != 0);//????? funfa
-		System.out.println("Alnalise lexica completa!!");
+		while(true){
+			Scanner entrada = new Scanner (System.in);
+			System.out.println("Indique 1 teste: (example '1', s para sair)");
+			String teste = entrada.nextLine();
+			if(teste == "s") break;
+			
+			Lexer lexer = new Lexer("inputs/teste"+teste+".txt");
+			Token tok = null;
+			
+			do{
+				tok = lexer.scan();
+				System.out.println(tok.toString());
+	
+			}while(tok.tag != 0);//????? funfa
+			System.out.println("Analise lexica completa!!\n\n");
+		}
 	}
 }
