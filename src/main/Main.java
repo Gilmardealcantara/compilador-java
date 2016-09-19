3925b72ab7b59e11b5bba7239b647525e01bdb60
dangon1 <d.aguiargoncalves@gmail.com>
2016-09-18 21:47:06 -0300

    Melhoria na visualização da saída

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,8 @@ import lexico.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 	
-		
+		// cont é string para evitar atribuição por referência entre ints do Java
+		String cont;
 		while(true){
 			Scanner entrada = new Scanner (System.in);
 			System.out.println("Indique 1 teste: (example '1', s para sair)");
@@ -17,11 +18,18 @@ public class Main {
 			if(teste.equals("s")) System.exit(0);
 			
 			Lexer lexer = new Lexer("inputs/teste"+teste+".txt");
+			lexer.line=1;
 			Token tok = null;
-			
+
+			System.out.print("\nLinha " + lexer.line + ":");
 			do{
+
+				cont = lexer.line+ "";
 				tok = lexer.scan();
-				System.out.println(tok.toString());
+				if(lexer.line == Integer.parseInt(cont))
+					System.out.print(tok.toString()+" ");
+				else
+					System.out.print("\nLinha " + lexer.line+ ": " + tok.toString());
 			}while(tok.tag != 0);//????? funfa
 			System.out.println("Analise lexica completa!!\n\n");
 		}
