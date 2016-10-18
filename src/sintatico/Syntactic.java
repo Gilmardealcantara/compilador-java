e41f83bd39430f3350886bdba8416d82f071d81c
gilmar <gilmar@localhost>
2016-10-17 23:51:55 -0200

    Add Syntactic class .

--- src/sintatico/Syntactic.java
+++ src/sintatico/Syntactic.java
@@ -6,20 +6,29 @@ import java.io.IOException;
 import lexico.*;
 
 public class Syntactic {
+	public Token tok = null;
+	Lexer lexer;
+	
+	public Syntactic() throws IOException {
+		this.lexer = new Lexer("inputs/teste1.txt");
+		this.tok = lexer.scan(); // traz primeiro token
+	}
 
-	public Syntactic() {
-		// TODO Auto-generated constructor stub
+	private void error(){
+		System.out.println("Correee negada....");
+		 System.exit(0);
 	}
 	
-	public void exec() throws IOException{
-		Lexer lexer = new Lexer("inputs/teste1.txt");
-		Token tok = null;
-		
-		do{
-	
-			tok = lexer.scan();
+	private void advance() throws IOException{
+		tok = this.lexer.scan();
+	}
 
-		}while(tok.tag != 0);
+	private void eat(Token t) throws IOException{
+		if(tok.tag == t.tag) this.advance();
+		else this.error();
+	}
 	
+	public void exec(){
+		//chama program function
 	}
 }
